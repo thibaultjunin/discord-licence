ce8d2cfb58eb38e556ad469467bc501fcdee469a
Thibault <thibault@stantabcorp.com>
2020-10-08 22:24:23 +0200

    Added new icons on specific periods

--- Logo.js
+++ Logo.js
@@ -1,13 +1,60 @@
 'use strict';
-const GUILD = "687978521796411441";
+const GUILD = "747824621486866612";
 const sharp = require('sharp');
 
 module.exports = class Logo{
     static load(client){
-        setTimeout(async () => {
-            let base = await sharp('./base.png')
-                .png()
-                .toBuffer();
+        setInterval(async () => {
+            let now = new Date();
+            if(now.getHours() != 0 && now.getMinutes() != 0){
+                return;
+            }
+
+            let base = null;
+            let addon = null;
+            if(now.getMonth() == 9){
+                // Halloween
+                addon = "./icons/citrouille.png"
+            }else if(now.getMonth() == 11 && (now.getDate() >= 10 && now.getDate() <= 28)){
+                // Noel
+                addon = "./icons/hiver.png"
+            }else if(now.getMonth() == 0 && now.getDate() <= 10){
+                // Nouvel an
+                addon = "./icons/nouvelan.png"
+            }else if(now.getMonth() == 1 && (now.getDate() >= 13 && now.getDate() <= 15)){
+                // St Valentin
+                addon = "./icons/ballons.png"
+            }else if(now.getMonth() == 3){
+                // Paques
+                addon = "./icons/paques.png"
+            }else if(now.getMonth() == 6 || now.getMonth() == 7){
+                // Vacances
+                addon = "./icons/ete.png"
+            }else{
+                base = await sharp('./base.png')
+                    .png()
+                    .toBuffer();
+            }
+
+            
+            if(addon != null){
+                let icon = await sharp(addon)
+                    .png()
+                    .resize(200)
+                    .toBuffer();
+                base = await sharp('./base2.png')
+                    .composite([{
+                        input: icon,
+                    }])
+                    .png()
+                    .toBuffer();
+            }
+
+            if(base == null){
+                console.error("oopsi")
+                return;
+            }
+            
             let image = await sharp({
                 create: {
                     width: 600,
@@ -21,13 +68,13 @@ module.exports = class Logo{
                 }])
                 .png()
                 .toBuffer();
-            
+
             client.guilds.fetch(GUILD).then(g => {
-                g.setIcon(image)
-                console.log("changed")
-            })
+                g.setIcon(image).catch(ex1 => console.error("Set Icon", ex1))
+            }).catch(ex2 => console.error("Fetch Guild", ex2))
+            client.user.setAvatar(image);
                 
-        }, 3600*1000*24);
+        }, 60000);
     }
 
     static getDayRGB(){
@@ -40,16 +87,16 @@ module.exports = class Logo{
         if(day < 256){
             return {
                 r: day,
-                g: day,
-                b: Math.floor(Math.random() * Math.floor(255)), // Random is always fun
+                g: Math.floor(Math.random() * Math.floor(255)), // Random is always fun
+                b: day,
                 alpha: 1
             }
         }else{
 
             return {
                 r: 365-day,
-                g: day%365,
-                b: Math.floor(Math.random() * Math.floor(255)), // Random is a lot of fun
+                g: Math.floor(Math.random() * Math.floor(255)), // Random is a lot of fun
+                b: day%365,
                 alpha: 1
             }
 
