4e3b8d68ee58bf6fd8b2a35e2707f10799dfb7f1
takitsu21 <dylannbatisse@gmail.com>
2021-09-01 14:57:06 +0200

    Add embed slash command

--- cogs/slash_commands.py
+++ cogs/slash_commands.py
@@ -14,7 +14,6 @@ class SlashCommands(commands.Cog):
     @cog_ext.cog_slash(
         name="cours",
         description="Ajoute un nouveau cours (Groupe d'un salon annonce, textuel et vocal)",
-        guild_ids=[747824621486866612],
         options=[
             manage_commands.create_option(
                 name="year",
@@ -46,6 +45,49 @@ class SlashCommands(commands.Cog):
     )
     async def _lessons(self, ctx: SlashContext, year, title):
         await botCommands.addChannelGroup(self.bot, ctx, year, title)
+
+    @cog_ext.cog_slash(
+        name="embed",
+        description="Créer un embed pour le rôle picker.",
+        options=[
+            manage_commands.create_option(
+                name="title",
+                description="Veuillez choisir le titre de l'embed.",
+                option_type=3,
+                required=True,
+                choices=[
+                    manage_commands.create_choice(
+                        name="Licence 1",
+                        value="L1"
+                    ),
+                    manage_commands.create_choice(
+                        name="Licence 2",
+                        value="L2"
+                    ),
+                    manage_commands.create_choice(
+                        name="Licence 3",
+                        value="L3"
+                    ),
+                    manage_commands.create_choice(
+                        name="Master 1",
+                        value="M1"
+                    ),
+                    manage_commands.create_choice(
+                        name="Master 2",
+                        value="M2"
+                    ),
+                ]
+            ),
+            manage_commands.create_option(
+                name="titre",
+                description="Nom de l'embed",
+                option_type=3,
+                required=True
+            )
+        ]
+    )
+    async def _embed(self, ctx: SlashContext, embed_title: str):
+        await botCommands.embed_command(self.bot, ctx, embed_title)
 
 
 def setup(bot):
